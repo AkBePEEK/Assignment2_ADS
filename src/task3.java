7ad3b21d2564ffaef1a185eed97be0bd680672c0
AkBePEEK <231338@astanait.edu.kz>
2024-04-07 20:01:43 +0500

    first

--- src/task3.java
+++ src/task3.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
+import static java.lang.Math.sqrt;
 
 public class task3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
+        System.out.println(prime(n));
+    }
+    public static String prime(int n){
+        for (int i=2;i<sqrt(n); i++){
+            if (n % i == 0) return "Composite";
+        }
+        return "Prime";
     }
 }
