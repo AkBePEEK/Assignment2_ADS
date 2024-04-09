cfa74a1af939f56182ac9d094cc970dbc8ccbc26
AkBePEEK <231338@astanait.edu.kz>
2024-04-10 01:43:07 +0500

    first

--- src/tasks/task1.java
+++ src/tasks/task1.java
@@ -2,13 +2,31 @@ package tasks;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class task1 {
-    public static void solution(Scanner sc){
-        int n = sc.nextInt(), b = sc.nextInt();
-        for (int i=0; i<n-1; i++){
+    /**
+     * This method finds the minimum value among a series of integers entered by the user.
+     * Time complexity: O(n), where n is the input number.
+     * @param sc Scanner object for user input
+     */
+    public static void solution(Scanner sc) {
+        // Read the total number of elements 'n' and the initial value of 'b' from input
+        int n = sc.nextInt(); // Total number of elements
+        int b = sc.nextInt(); // Initial value of 'b'
+
+        // Loop through the elements, starting from the second element (index 0-based)
+        for (int i = 0; i < n - 1; i++) {
+            // Read the next element 'm' from input
             int m = sc.nextInt();
-                if (b>m) b = m;
+
+            // Check if the current element 'm' is less than the current minimum 'b'
+            // If so, update the minimum value 'b' to the value of 'm'
+            if (b > m)
+                b = m;
         }
+
+        // Print the final minimum value 'b'
         System.out.println(b);
     }
 }
--- src/tasks/task2.java
+++ src/tasks/task2.java
@@ -3,13 +3,32 @@ package tasks;
 import java.util.Scanner;
 
 public class task2 {
+    /**
+     * This method calculates the average of 'n' numbers entered by the user.
+     * Time complexity: O(n), where n is the input number.
+     *
+     * @param sc Scanner object for user input
+     */
     public static void solution(Scanner sc) {
-        int n = sc.nextInt();
+        // Read the total number of elements 'n' from input
+        int n = sc.nextInt(); // Total number of elements
+
+        // Initialize a variable to store the sum of all numbers
         float ans = 0;
-        for (int i=0; i<n; i++){
+
+        // Loop through 'n' numbers entered by the user
+        for (int i = 0; i < n; i++) {
+            // Read the next number from input
             int num = sc.nextInt();
+
+            // Add the number to the sum
             ans += num;
         }
-        System.out.println(ans/4);
+
+        // Calculate the average by dividing the sum by the total number of elements
+        float average = ans / n;
+
+        // Print the average
+        System.out.println(average);
     }
 }
